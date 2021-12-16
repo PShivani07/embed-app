4859a6d54d6b801b57036b2d843d200c78e0eac6
shipalan <shipalan@sinkhole.esl.cisco.com>
2021-12-16 10:42:01 +0530

    my project

---
+++ data.js
@@ -0,0 +1,17 @@
+var access_key = 'Bearer Mjc5MDhlMjItOGFmMi00M2VlLWI2OWEtY2QwNWRjMGY4MjE3ODgzMWMyNTAtZjgy_PF84_1eb65fdf-9643-417f-9974-ad72cae0e10f'
+
+// var data = {}
+// const address = fetch('https://webexapis.com/v1/rooms', {
+//                         method: 'GET',
+//                         headers:{
+//                             "Authorization": "Bearer NTIyNjVjYzUtMmVhYi00MzIxLWIyNTQtOWVmYTQ1MmFhN2JkNjY0ZTgxZTMtMzJm_PF84_1eb65fdf-9643-417f-9974-ad72cae0e10f",
+//                             "Content-Type": "application/json"
+//                         }
+//                     })
+//                     .then((response) => response.json())
+
+// const printAddress = async () => {
+//                             data = await address;
+//                             console.log(data);
+//                             return data
+//                             };
